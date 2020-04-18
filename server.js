a8ac8abd703277d0329261317860cbba16061025
Moojig <moojigc@gmail.com>
2020-04-18 11:56:02 -0400

    Added cors module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+var cors = require('cors');
 // const Reservation = require('./Reservation')
 
 const app = express();
